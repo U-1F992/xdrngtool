46a0ef0b5f826346d98d69c6851afc0dea587b5a
Koutaro Mukai <mukai.k1011k@outlook.jp>
2022-09-21 22:08:37 +0900

    roughly

--- xdhelper/helper.py
+++ xdhelper/helper.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from typing import List, Optional, Tuple, TypeAlias
 
-from xddb import EnemyTeam, PlayerTeam
+from xddb import PlayerTeam, EnemyTeam, generate_quick_battle
 from lcg import LCG
 
 TeamPair: TypeAlias = Tuple[Tuple[PlayerTeam, int, int], Tuple[EnemyTeam, int, int]]
@@ -22,6 +22,10 @@ LEFTOVER = timedelta(minutes=1)
 # いますぐバトルにファイヤーを出した場合の消費数/秒
 # 一般的には3713.6と言われていますが（出典不明）、3842程度で上手くいきます。
 ADVANCES_PER_SECOND_BY_MOLTRES = 3842
+
+ADVANCES_BY_CHANGE_SETTING = 40
+ADVANCES_BY_WRITE_REPORT = 63
+ADVANCES_BY_WATCHING_STEPS = 2
 
 def get_wait_time(
     current_seed: int,
@@ -56,4 +60,25 @@ def get_route(
     tsv: int = 0x10000,
     opts: Optional[Tuple[int, int]] = None
 ) -> Tuple[List[TeamPair], int, int, int, int]:
-    pass
+    """消費経路を算出します。
+
+    Args:
+        current_seed (int): 現在のseed
+        target_seed (int): 目標のseed
+        tsv (int, optional): TSV。正確に指定されない場合、実際のいますぐバトルの生成結果および回数は異なる可能性が生じます。 Defaults to 0x10000.
+        opts (Optional[Tuple[int, int]], optional): ロード後の使用する消費数（ロード時の強制消費数、もちものを開く際の消費数）。 Defaults to None.
+
+    Returns:
+        Tuple[List[TeamPair], int, int, int, int]: 消費経路（いますぐバトルの生成リスト、設定変更回数、レポート回数、もちものを開く回数、腰振りを見る回数）
+    """
+
+    # optsがNoneの場合（ロードしない）
+    # 40で割り切れるようにいますぐバトルの生成を切り上げる。
+
+    # optsがNoneでない場合（ロードする）
+    # 40a + by_loading + 63b + by_opening_items*c + 2d で表す。
+    
+    lcg = LCG(current_seed)
+    result = generate_quick_battle(lcg, tsv)
+
+    return ([(PlayerTeam.Mewtwo, 100, 100), (EnemyTeam.Articuno, 100, 100)], 0, 0, 0, 0)
