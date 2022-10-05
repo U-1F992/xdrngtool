004d928fbc8533bfeadadaa9e87bb4797c73f029
Koutaro Mukai <mukai.k1011k@outlook.jp>
2022-10-05 21:44:51 +0900

    test

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -1,16 +1,25 @@
 from datetime import timedelta
+from time import sleep
 from typing import Optional
 from xddb import EnemyTeam, PlayerTeam, QuickBattleSeedSearcher, XDDBClient
 from xdrngtool import execute_automation, TargetSelector, CurrentSeedSearcher, TeamPair, SeedAdjuster
 
-class GenerateNextTeamPair():
-    def run(self) -> TeamPair:
-        pass
+
+def _indicate(obj):
+    print(f"{obj.__class__.__name__} called")
+    sleep(0.3)
+
 class TransitionToQuickBattle():
     def run(self):
+        _indicate(self)
+        pass
+class GenerateNextTeamPair():
+    def run(self) -> TeamPair:
+        _indicate(self)
         return ((PlayerTeam.Mewtwo, 100, 100), (EnemyTeam.Articuno, 100, 100))
 class EnterWaitAndExitQuickBattle():
     def run(self, wait_time: timedelta):
+        _indicate(self)
         pass
 class SetCursorToSetting():
     def run(self):
@@ -41,8 +50,8 @@ client = XDDBClient()
 searcher = QuickBattleSeedSearcher(client, tsv) if tsv is not None else QuickBattleSeedSearcher(client)
 
 operations = (
-    GenerateNextTeamPair(),
     TransitionToQuickBattle(),
+    GenerateNextTeamPair(),
     EnterWaitAndExitQuickBattle(),
     SetCursorToSetting(),
     ChangeSetting(),
@@ -53,9 +62,9 @@ operations = (
     WatchSteps(),
 )
 
-current_seed_searcher = CurrentSeedSearcher(searcher, operations[0])
+current_seed_searcher = CurrentSeedSearcher(searcher, operations[1])
 
-target_selector = TargetSelector(current_seed_searcher, operations[1])
-seed_adjuster = SeedAdjuster(current_seed_searcher, operations[0], *operations[2:], tsv, advances_by_opening_items)
+target_selector = TargetSelector(current_seed_searcher, operations[0])
+seed_adjuster = SeedAdjuster(current_seed_searcher, *operations[1:], tsv, advances_by_opening_items)
 
 execute_automation(target_selector, seed_adjuster, [])
